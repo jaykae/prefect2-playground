ac5dc1762ea764c57a925d31042261210ac993ac
Jeremy Knickerbocker <jknickerbocker@oncologyanalytics.com>
2023-07-20 22:33:04 -0400

    Maybe ready for a demo?

--- flows/summarize_github.py
+++ flows/summarize_github.py
@@ -76,7 +76,6 @@ def filter_data(data: dict) -> dict:
 @task
 def persist_results(data: dict, artifact_name: str):
     artifact_key = "summarized-data" if artifact_name is None else artifact_name
-    json.dumps(data, default=str)
     create_table_artifact(
         table=json.loads(json.dumps(data, default=str)),
         key=artifact_key,
